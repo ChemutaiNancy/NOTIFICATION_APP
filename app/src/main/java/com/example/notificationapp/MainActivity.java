b864d1e53154e721553a8607c984d05c23d8c34b
Nancy <chemutain@gmail.com>
2020-04-10 16:47:41 +0300

    Added notification manager

--- app/src/main/java/com/example/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/notificationapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.notificationapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void myButtonClick(View view) {
+        notification.setSmallIcon(R.drawable.apple);
+        notification.setTicker("This is a ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("This is the title");
+        notification.setContentText("Here is the text");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(UNIQUE_ID, notification.build());
     }
 }
