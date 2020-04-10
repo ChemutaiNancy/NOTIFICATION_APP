554ff5df3667dfc3c0e2b537ef2c4b3d8ca255e2
Nancy <chemutain@gmail.com>
2020-04-10 16:36:15 +0300

    Set auto cancel

--- app/src/main/java/com/example/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/notificationapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.notificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    NotificationCompat.Builder notification;
+    private static final int UNIQUE_ID = 40111;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notification = new NotificationCompat.Builder(this);
+        //autocancel notification
+        notification.setAutoCancel(true);
+    }
+
+    public void myButtonClick(View view) {
     }
 }
